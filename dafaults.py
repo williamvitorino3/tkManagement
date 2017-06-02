9de78291aa0647e40aaf9c4126d9d356423ffc43
Aluno <aluno@reinaldo-Linux>
2017-06-02 15:30:43 -0300

    Problema com espaço no nomes dos tipos de atendimento resolvido.

--- dafaults.py
+++ dafaults.py
@@ -38,4 +38,4 @@ fg_widget = "#000000"
 insert_bg = fg_widget
 justify_text = "center"
 ATENDIMENTOS = ["Cortesia", "Particular", "Amil", "Cafaz", "Cassi", "Geap",
-                "Saúde Caixa", "Unimed"]
+                "Saúde_Caixa", "Unimed"]
