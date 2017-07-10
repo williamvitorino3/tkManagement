d4820abc6420556475ab0e8c18aa398cbc99670a
William Vitorino <williamvitorino3@gmail.com>
2017-07-10 20:18:17 -0300

    DocStrinf do script adicionado.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import tkinter as tk
+"""Script de inicialização do projeto."""
+
+from tkinter import Tk
 from view.tela import Janela
 
 
-root = tk.Tk()
-root.title("tkMed")
+root = Tk()
+root.title("tkMeanagement")
 root.resizable(False, False)
 root.geometry("1366x768+0+0")
 # root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 tela = Janela(root)
 root.mainloop()
+
