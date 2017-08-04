2b6633938ecfc1e0b390119934f603dd2cf22992
William Vitorino <williamvitorino3@gmail.com>
2017-08-04 16:27:29 -0300

    with adicionado nas conexões com o banco.

--- model/cliente_db.py
+++ model/cliente_db.py
@@ -8,19 +8,17 @@ from datetime import datetime
 
 def criar_banco():
     """Cria o banco de dados."""
-    conn = sqlite3.connect('clientes.db')
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS clientes (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            nome VARCHAR(100) NOT NULL,
-            data_atendimento varchar(15) NOT NULL,
-            data_nascimento varchar(15) NOT NULL,
-            tipo_atendimento varchar(50) NULL
-    );
-    """)
-    conn.close()
+    with sqlite3.connect('clientes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS clientes (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nome VARCHAR(100) NOT NULL,
+                data_atendimento varchar(15) NOT NULL,
+                data_nascimento varchar(15) NOT NULL,
+                tipo_atendimento varchar(50) NULL
+        );
+        """)
 
 
 def _now_(formato="%d/%m/%Y"):
@@ -29,63 +27,60 @@ def _now_(formato="%d/%m/%Y"):
 
 def inserir(nome="None", data_nascimento="None", tipo_atendimento="None"):
     """Insere dados no banco."""
-    conn = sqlite3.connect('clientes.db')
-    cursor = conn.cursor()
-    cursor.execute("""
-    INSERT INTO clientes
-                (data_atendimento, nome, data_nascimento, tipo_atendimento)
-    VALUES (?, ?, ?, ?)
-    """, (_now_(), nome, data_nascimento, tipo_atendimento))
+    with sqlite3.connect('clientes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        INSERT INTO clientes
+                    (data_atendimento, nome, data_nascimento, tipo_atendimento)
+        VALUES (?, ?, ?, ?)
+        """, (_now_(), nome, data_nascimento, tipo_atendimento))
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
 
 def buscar_all():
     """Busca dados no banco."""
-    conn = sqlite3.connect('clientes.db')
-    cursor = conn.cursor()
-    cursor.execute("""SELECT * FROM clientes ORDER BY nome""")
-    resultado = cursor.fetchall()
-    conn.close()
+    resultado = None
+    with sqlite3.connect('clientes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("""SELECT * FROM clientes ORDER BY nome""")
+        resultado = cursor.fetchall()
     return resultado
 
 
 def buscar(nome):
     """Busca dados no banco."""
-    conn = sqlite3.connect('clientes.db')
-    cursor = conn.cursor()
-    cursor.execute("""
-    SELECT * FROM clientes
-    WHERE nome like ?
-    ORDER BY nome
-    """, ('%' + nome + '%', ))
-    resultado = cursor.fetchall()
-    conn.close()
+    with sqlite3.connect('clientes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT * FROM clientes
+        WHERE nome like ?
+        ORDER BY nome
+        """, ('%' + nome + '%', ))
+        resultado = cursor.fetchall()
     return resultado
 
 
 def remover(id_cliente):
     """Remove do banco o cliente com o ID recebido."""
-    conn = sqlite3.connect('clientes.db')
-    cursor = conn.cursor()
-    cursor.execute("""DELETE FROM clientes WHERE id = ?""", (id_cliente,))
-    conn.commit()
-    conn.close()
+    with sqlite3.connect('clientes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("""DELETE FROM clientes WHERE id = ?""", (id_cliente,))
+        conn.commit()
 
 
 def atualizar(id_cliente, nome, data_nascimento, tipo_atendimento):
     """Atualiza no banco os dados do cliente com o ID recebido."""
-    conn = sqlite3.connect('clientes.db')
-    cursor = conn.cursor()
-    cursor.execute("""
-    UPDATE clientes
-    SET data_atendimento = ?, nome = ?,
-        data_nascimento = ?, tipo_atendimento = ?
-    WHERE id = ?
-    """, (_now_(), nome, data_nascimento, tipo_atendimento, id_cliente))
-    conn.commit()
-    conn.close()
+    with sqlite3.connect('clientes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        UPDATE clientes
+        SET data_atendimento = ?, nome = ?,
+            data_nascimento = ?, tipo_atendimento = ?
+        WHERE id = ?
+        """, (_now_(), nome, data_nascimento, tipo_atendimento, id_cliente))
+        conn.commit()
 
 
 def add_clientes_teste():
